7eb3c5be452651e36904640c42e21d40cb1e8747
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2024-10-18 19:54:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BudetTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            string? value = Console.ReadLine();
+            if (value != null)
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Value was null");
+            }
+        }
+    }
+}
